77d00a7f4dd331a00c468124ea6845defbfae7ad
Sergey Kiskin <sector1200@gmail.com>
2022-10-21 16:40:56 +0500

    fine weather nav bar

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav class="container flex flex-col sm:flex-row items-center gap-4 py-6">
+      <RouterLink :to="{ name: 'home'}">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-cloud text-2xl"></i>
+          <p class="text-2xl">Fine Weather</p>
+        </div>
+      </RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
