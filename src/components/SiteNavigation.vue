31b90cb4f1021b00c9c5aeb2c1c3017e4734ed1c
Sergey Kiskin <sector1200@gmail.com>
2022-10-25 13:25:55 +0500

    More pretty title image

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,9 +1,10 @@
 <template>
   <header class="sticky top-0 bg-weather-primary shadow-lg">
     <nav class="container flex flex-col sm:flex-row items-center gap-4 py-6">
-      <RouterLink :to="{ name: 'home'}">
+      <RouterLink :to="{ name: 'home' }">
         <div class="flex items-center gap-3">
-          <i class="fa-solid fa-cloud text-2xl"></i>
+          <img src="@/assets/weather-pictures/01d.png" class="w-24 h-24"/>
+          <!-- <i class="fa-solid fa-cloud text-2xl"></i> -->
           <p class="text-2xl">Fine Weather</p>
         </div>
       </RouterLink>
