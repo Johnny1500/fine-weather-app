a26131947c904bc9782021295b04797b9586f24d
Sergey Kiskin <sector1200@gmail.com>
2022-10-28 13:33:33 +0500

    Working loading indicator

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,7 @@
     <div
       class="bg-weather-primary flex flex-col justify-center items-start rounded-xl"
       :class="`${
-        cityInput.length > 0
-          ? 'bg-weather-secondary'
-          : 'bg-weather-primary'
+        cityInput.length > 0 ? 'bg-weather-secondary' : 'bg-weather-primary'
       }`"
     >
       <div class="input-wrapper w-full">
@@ -13,13 +11,10 @@
           type="text"
           placeholder="Search for city"
           v-model="cityInput"
-          class="w-full text-xl -ml-4 px-10 py-2 rounded-full"
-          :class="`${
-            cityInput.length > 0 ? 'outline-none' : 'outline-1'
-          }`"
+          class="w-full text-xl -ml-4 px-10 py-2 rounded-full outline-none"
         />
       </div>
-      <div v-show="cityInput.length > 0">
+      <div v-show="cityInput.length > 0" class="w-full">
         <ul v-if="!loadingCities">
           <li
             v-for="item in selectedGeoDBCities"
@@ -29,7 +24,29 @@
             {{ item["name"] }}
           </li>
         </ul>
-        <div v-else>Loading...</div>
+        <div v-else class="flex items-center justify-center pb-3">          
+          <svg
+            class="animate-spin -ml-1 mr-3 h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          <p class="text-xl">Loading...</p>          
+        </div>
       </div>
     </div>
   </div>
@@ -81,12 +98,12 @@ async function searchFromGeoDB(newCityInput: string) {
 
 const searchFromGeoDBDebounced = debounce(searchFromGeoDB, 1200);
 
-
 watch(cityInput, (...args) => {
   loadingCities.value = true;
   // eslint-disable-next-line
   // @ts-ignore
-  return searchFromGeoDBDebounced(...args)});
+  return searchFromGeoDBDebounced(...args);
+});
 </script>
 
 <style lang="scss" scoped>
