35bdb6bda4d4306c8c41af1aad2951d402a3e90f
Sergey Kiskin <sector1200@gmail.com>
2022-12-02 13:59:57 +0500

    Debounce function in utility.ts

---
+++ src/utility.ts
@@ -0,0 +1,9 @@
+export const debounce = function (func: any, ms: number) {
+  let timeout: number;
+  return function () {
+    clearTimeout(timeout);
+    // eslint-disable-next-line
+    // @ts-ignore
+    timeout = setTimeout(() => func.apply(this, arguments), ms);
+  };
+};
