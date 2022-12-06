7a5a3d64f324f5ecbd32b7d7eb93e5daef9c499c
Sergey Kiskin <sector1200@gmail.com>
2022-12-06 14:59:38 +0500

    City route first option

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,17 +1,38 @@
 <template>
-    <div>
-        {{  $route.params.cityname }}
-        {{  $route.params.lat }}
-        {{  $route.params.lon }}
-    </div>
+  <div>
+    {{ $route.params.cityname }}
+    {{ $route.params.lat }}
+    {{ $route.params.lon }}
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { OPEN_WEATHER_API_URL, OPEN_WEATHER_API_KEY } from "../api";
+import { onMounted } from "vue";
+import router from "../router";
+
+onMounted(async () => {
+  // console.log('route', router);
+
+  try {
+    const lat = router.currentRoute.value.params.lat;
+    const lon = router.currentRoute.value.params.lon;
+
+    console.log("lat ===", lat);
+    console.log("lon ===", lon);
+
+    const response = await fetch(`${OPEN_WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}`);
+    const weatherData = await response.json();
+
+    console.dir(weatherData);
 
 
+  } catch (error) {
+
+    console.log('error during fetch', error);
+
+  }
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
