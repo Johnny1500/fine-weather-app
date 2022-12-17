2b57f48c52294ad7731727aaa4aaea72b0ded711
Sergey Kiskin <sector1200@gmail.com>
2022-12-17 17:55:49 +0500

    Without api keys

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 export const geoOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "d23a321856msh76b5b82f446a0b2p18d690jsn8f15341160f4",
+    "X-RapidAPI-Key": "",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const OPEN_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const OPEN_WEATHER_API_KEY = "19792952d501cbbea058ceddb2892f38";
+export const OPEN_WEATHER_API_KEY = "";
