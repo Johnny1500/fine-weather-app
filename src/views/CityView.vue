917301fffd53db2456976baa0f44880fba777424
Sergey Kiskin <sector1200@gmail.com>
2022-12-19 22:27:33 +0500

    City view layout pre markup

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,19 +1,38 @@
 <template>
   <div>
-    <div v-if="!loadingWeatherData">
-      <p class="text-xl">Current weather</p>
-      <p class="text-xl">{{ currentWeatherDataForRender?.feels_like  }}</p>
-    </div>
-    <div v-else>
-      <p class="text-xl">Loading...</p>
+    <div
+      class="border-4 border-dashed border-red-500 w-full mx-auto mt-10 p-2 md:w-3/4"
+    >
+      <div v-if="!loadingWeatherData">
+        <section class="border-4 border-dashed border-blue-500 rounded-lg p-2">
+          <h2 class="text-2xl">Current weather</h2>
+          <div class="border-4 border-dashed border-purple-500 h-56 pt-2">
+            <p class="text-xl">{{ currentWeatherDataForRender?.feels_like }}</p>
+          </div>
+        </section>
+        <section class="border-4 border-dashed border-green-500 rounded-lg mt-3 p-2">
+          <h2 class="text-2xl">Forecast</h2>
+          <div class="border-4 border-dashed border-yellow-500 hidden md:block md:h-56 text-xl pt-2">
+            Forecast weather
+          </div>
+          <div class="border-4 border-dashed border-yellow-500 block md:hidden text-xl mt-2">
+            <p v-for="day in Array.from({ length:5 })" class="border-4 border-dashed border-orange-500 py-2">
+              Forecast weather2
+            </p>
+          </div>
+        </section>
+      </div>
+      <div v-else>
+        <p class="text-xl">Loading...</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import type { Ref } from "vue";
-import type {  
-  CurrentWeatherDataForRender,  
+import type {
+  CurrentWeatherDataForRender,
   ForecastItemWeatherDataForRender,
 } from "../interfaces";
 
@@ -35,14 +54,20 @@ let forecastWeatherDataForRender: Ref<ForecastItemWeatherDataForRender[][]> =
 
 onMounted(async () => {
   loadingWeatherData.value = true;
-  
-  currentWeatherDataForRender.value = await useCurrentWeatherDataForRender(lat, lon);
-  forecastWeatherDataForRender.value = await useForecastWeatherDataForRender(lat, lon);  
-   
+
+  currentWeatherDataForRender.value = await useCurrentWeatherDataForRender(
+    lat,
+    lon
+  );
+  forecastWeatherDataForRender.value = await useForecastWeatherDataForRender(
+    lat,
+    lon
+  );
+
   console.log(
     "currentWeatherDataForRender.value === ",
     currentWeatherDataForRender.value
-  );  
+  );
   console.log(
     "forecastWeatherDataForRender.value === ",
     forecastWeatherDataForRender.value
