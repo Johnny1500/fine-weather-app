f260d883a82ea43c460c8408ee59f187fc34ad88
Sergey Kiskin <sector1200@gmail.com>
2023-01-23 16:06:32 +0500

    Half-working transition of carousel 's navigation

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -5,17 +5,19 @@
     @mouseout="showNavigation = false"
   >
     <slot :currentSlide="currentSlide"></slot>
-    <div
-      class="absolute w-full h-full flex items-center justify-center px-2"
-      v-show="showNavigation"
-    >
-      <div class="flex flex-1">
-        <i @click="prevSlide" class="fas fa-chevron-left"></i>
+    <transition name="slide-navigation">
+      <div
+        class="absolute w-full h-full flex items-center justify-center px-2"
+        v-show="showNavigation"
+      >
+        <div class="flex flex-1">
+          <i @click="prevSlide" class="fas fa-chevron-left"></i>
+        </div>
+        <div class="flex flex-1 justify-end">
+          <i @click="nextSlide" class="fas fa-chevron-right"></i>
+        </div>
       </div>
-      <div class="flex flex-1 justify-end">
-        <i @click="nextSlide" class="fas fa-chevron-right"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -53,6 +55,21 @@ const prevSlide = () => {
 </script>
 
 <style scoped>
+.slide-navigation-enter-active {
+  transition: opacity 1s ease-in-out;
+}
+.slide-navigation-leave-active {
+  transition: opacity 1s ease-in-out;
+}
+.slide-navigation-enter-from,
+.slide-navigation-leave-to {
+  opacity: 0 !important;
+}
+.slide-navigation-enter-to,
+.slide-navigation-leave-from {
+  opacity: 1 !important;
+}
+
 i {
   cursor: pointer;
   display: flex;
