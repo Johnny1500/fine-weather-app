9625ffffdd8da986c83e73e306cfa3058828a833
Sergey Kiskin <sector1200@gmail.com>
2023-01-23 16:06:48 +0500

    Working slide transition

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
-        <slot></slot>
-    </div>
+  <div>
+    <transition name="slide">
+      <slot></slot>
+    </transition>
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
-
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.7s ease-in-out;
+}
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+}
+.slide-enter-to,
+.slide-leave-from {
+  opacity: 1;
+}
 </style>
